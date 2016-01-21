e8c010215f235d83b6c211e0d3d0d14be5d9f92b
SABRINA TEE <sabrinatee@me.com>
2016-01-21 11:37:18 -0800

    Completed testing

--- sabrina/test/server_test.js
+++ sabrina/test/server_test.js
@@ -1,35 +1,21 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const server = require(__dirname + '/../server');
+const server = require(__dirname + '/temp-server');
 chai.use(chaiHttp);
 const expect = chai.expect;
 const request = chai.request;
-var origin = 'localhost:3000';
-var uri = '/jsonSend';
+var origin = 'localhost:8000';
+var uri = '/post';
 
-describe('rest functionality of server', () => {
+describe('rest functionality of the server', () => {
   after(() => server.close());
 
   it('should respond to a POST request', (done) => {
-    var jsonName = {"name": "sab"};
     request(origin)
       .post(uri)
-      .send(jsonName)
       .end((err, res) => {
         expect(err).to.eql(null);
-        expect(res.body).to.eql(jsonName);
-        done();
-      });
-  });
-
-  it('should respond to an invalid JSON POST request', (done) => {
-    var jsonName = 'sab';
-    request(origin)
-      .post(uri)
-      .send(jsonName)
-      .end((err, res) => {
-        expect(err).to.eql(null);
-        expect(res.body.msg).to.eql('Invalid JSON!');
+        expect(res.text).to.eql('Got a POST request');
         done();
       });
   });
