4c00c114aec633619ad4e497ac8385728828a7ab
Yueyue07 <xww.qyy@gmail.com>
2016-01-21 12:31:56 -0800

    add integration test

--- yueyueqin/test/integration_test.js
+++ yueyueqin/test/integration_test.js
@@ -5,13 +5,12 @@ chai.use(chaiHttp);
 const expect = chai.expect;
 const request = chai.request;
 var origin = 'localhost:3000';
-var uri = '/testpost';
+var url = '/testpost';
 
 describe('Test Server', () => {
-
-  it('should save post message in req.body', (done) => {
+  it('should post JSON data ', (done) => {
     request(origin)
-      .post(uri)
+      .post(url)
       .send('{ "name": "yueyue"}')
       .end((err,res) => {
         console.log(res.statusCode);
@@ -20,6 +19,21 @@ describe('Test Server', () => {
         expect(err).to.eql(null);
         expect(JSON.parse(res.text).name).to.eql('yueyue');
         expect(res.statusCode).to.eql(200);
+        done();
+      });
+  });
+
+  it('should return 500 if parsing data is not JSON', (done) => {
+    request(origin)
+      .post(url)
+      .send('yueyue')
+      .end((err,res) => {
+        console.log(res.statusCode);
+        console.log(res.text);
+        console.log(JSON.parse(res.text));
+        expect(err).to.eql(null);
+        expect(JSON.parse(res.text).error).to.eql('invalid json');
+        expect(res.statusCode).to.eql(500);
         done();
       });
   });
