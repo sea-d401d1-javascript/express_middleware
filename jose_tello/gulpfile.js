c61f8d1ccbe500c299b32dad15a2b941adf7a914
Jose Tello <josectello@gmail.com>
2016-01-20 21:10:07 -0800

    add gulpfile

--- jose_tello/gulpfile.js
+++ jose_tello/gulpfile.js
@@ -3,3 +3,20 @@ var gulp   = require('gulp'),
   eslint   = require('gulp-eslint'),
   chai     = require('chai'),
   chaiHTTP = require('chai-http');
+
+  gulp.task('lint', () => {
+    return gulp.src(['./*.js', './test/*.js'])
+      .pipe(eslint()) // uses .eslintrc file in root directory
+      .pipe(eslint.format());
+  });
+
+  gulp.task('test', () => {
+    return gulp.src(['./test/*.spec.js'])
+      .pipe(mocha());
+  });
+
+  gulp.task('watch', () => {
+    return gulp.watch(['./*.js', './test/*.js', '!node_modules/**', '!package.json'])
+  });
+
+  gulp.task('default', ['lint', 'test', 'watch']);
