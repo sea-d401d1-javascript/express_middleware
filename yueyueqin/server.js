601044f107d37c44b9c80e9e78ed25fdbcce3162
Yueyue07 <xww.qyy@gmail.com>
2016-01-21 12:31:17 -0800

    modify server.js file

--- yueyueqin/server.js
+++ yueyueqin/server.js
@@ -2,9 +2,8 @@ const express = require('express');
 const app = express();
 var parseJSON =  require(__dirname + '/lib/parseJSON');
 
-app.use('/post',parseJSON);
 
-app.post('/post',function(req,res,next){
+app.post('/post',parseJSON,(req,res,next) => {
   console.log(req.body);
   res.status(200).json(req.body);
 });
