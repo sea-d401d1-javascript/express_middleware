b56d0c41a68afa38ebe29dc2b018ecd2b0551108
Yueyue07 <xww.qyy@gmail.com>
2016-01-21 16:14:56 -0800

    add unit test for parseJSON file

--- yueyueqin/test/unit_test.js
+++ yueyueqin/test/unit_test.js
@@ -1,69 +1,35 @@
 const chai =  require('chai');
-const chaiHttp = require('chai-http');
-chai.use(chaiHttp);
 const expect = chai.expect;
-const request = chai.request;
+const fs = require('fs');
+const parseJSON = require(__dirname + '/../lib/parseJSON');
 
-const server = require(__dirname + '/test_server');
-const util = require('util');
-const Stream  = require('stream');
-
-const express = require('express');
-const app = express();
-
-var origin = 'localhost:3000';
-var url = '/testpost';
-
-var Req = function() {
-
-};
-util.inherits(Req,Stream);
 
 describe('parse JSON', () => {
 
   it('should send JSON data', (done) => {
+    var req = fs.createReadStream(__dirname + '/valid_json.json');
+    parseJSON(req, null, function() {
+      expect(req.body.name).to.equal('yueyue');
+      done();
+    });
+  });
 
-    var jsonString = '';
-    Req.prototype.data = function(){
-      return function(chunckData) {
-        jsonString +=  chunckData.toString();
-      };
+  it('should not parse invalid json', (done) => {
+    var req = fs.createReadStream(__dirname + '/invalid_json.json');
+    var res = {
+      status: function(statusCode) {
+        expect(statusCode).to.equal(400);
+        return {
+          json: function(obj) {
+            expect(obj.error).to.equal('invalid json');
+            done();
+          }
+        };
+      }
     };
-    Req.prototype.end = function() {
-      expect(jsonString).to.equal('{"name":"yueue"}');
-    };
-    var req = new Req();
-
-    request(origin)
-      .post(url)
-      .send('{ "name": "yueyue"}')
-      .end((err,res) => {
-        console.log(res.statusCode);
-        console.log(res.text);
-        console.log(JSON.parse(res.text));
-        req.data();
-        req.end();
-        //expect(err).to.eql(null);
-        //expect(JSON.parse(res.text).name).to.eql('yueyue');
-        //expect(res.statusCode).to.eql(200);
-        done();
-      });
-
-    // app.post('/testpost',function(req,res,next) {
-    //   req.data();
-    //   req.on();
-    //   next();
-    // });
-    //
-    // app.post(function(req,res,next){
-    //   console.log(res);
-    //   expect(res.statusCode).to.eql(400);
-    // });
-    //
-    // app.listen(3000, () => {
-    //   console.log('server up');
-    // });
-
+    parseJSON(req,res,function() {
+      throw new Error('called next function if error');
+    });
   });
 
 });
